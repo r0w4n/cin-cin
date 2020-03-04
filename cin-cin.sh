4107bc25ddc1451c6f51326d26132f7044bb543c
r0w4n <r0w4n@users.noreply.github.com>
2020-03-04 23:06:42 +0000

    Update cin-cin.sh

--- cin-cin.sh
+++ cin-cin.sh
@@ -1,32 +1,30 @@
-
-#!/bin/bash
+!/bin/bash
 
 main() {
-    source settings.cnf
+    source "${BASH_SOURCE%/*}/settings2.cnf"
 
     for camera in "${cameras[@]}"
     do
-      	createFirst $camera
+      	createSequenceFile $camera
         createSequence $camera
-        joinVideo $camera
         publish $camera
         cleanUp $camera
     done
 }
 
+createSequenceFile() {
+   echo -e "file '$(getLastImage $camera)'\nduration 5" >> $workingDir$camera.txt
+
+   duration=$(getSequenceFrameRate $camera)
+   for file in $(ls -1 $workingDir$camera-*.$format); do
+      echo "file '$file'" >> $workingDir$camera.txt
+      echo duration $duration >> $workingDir$camera.txt
+   done
+}
+
 createSequence() {
     camera=$1
-    /usr/bin/ffmpeg -framerate $(getSequenceFrameRate) -pattern_type glob -i "$workingDir$camera-*.$format" -c:v libx264 -r 30 -pix_fmt yuv420p -y $workingDir$camera-sequence.mp4
-}
-
-createFirst() {
-    camera=$1
-    /usr/bin/ffmpeg -loop 1 -i $(getLastImage $camera) -c:v libx264 -t 5 -r 30 -pix_fmt yuv420p -y $workingDir$camera-first.mp4
-}
-
-joinVideo() {
-    camera=$1
-    /usr/bin/MP4Box $workingDir$camera-first.mp4 -cat $workingDir$camera-sequence.mp4 -out $workingDir$camera-output.mp4
+    /usr/bin/ffmpeg -f concat -i $workingDir$camera.txt -vsync vfr -pix_fmt yuv420p $workingDir$camera-output.mp4 -y
 }
 
 publish() {
@@ -36,8 +34,7 @@ publish() {
 
 cleanUp() {
     camera=$1
-    /bin/rm $workingDir$camera-sequence.mp4
-    /bin/rm $workingDir$camera-first.mp4
+    /bin/rm $workingDir$camera.txt
 }
 
 getLastImage() {
@@ -45,7 +42,8 @@ getLastImage() {
 }
 
 getSequenceFrameRate() {
-    /usr/bin/bc <<< "scale=2; $(getSequenceCount)/$videoLength"
+    camera=$1
+    /usr/bin/bc -l <<< "scale=2; $videoLength/$(getSequenceCount $camera)" | /usr/bin/awk '{printf "%.4f\n", $0}'
 }
 
 getSequenceCount() {
@@ -54,4 +52,3 @@ getSequenceCount() {
 }
 
 main "$@"
-
