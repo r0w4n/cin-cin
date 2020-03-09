548c28e464bcdd4920ce259b35d8a08e743eccbe
r0w4n <r0w4n@users.noreply.github.com>
2020-03-09 19:39:53 +0000

    Update cin-cin.sh

--- cin-cin.sh
+++ cin-cin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main() {
-    source "${BASH_SOURCE%/*}/settings2.cnf"
+    source "${BASH_SOURCE%/*}/settings.cnf"
 
     for camera in "${cameras[@]}"
     do
@@ -17,22 +17,19 @@ createSequenceFile() {
 
    for file in $(find $workingDir -type f -newermt "$(/bin/date +"%Y-%m-%d") 08:00:00" ! -newermt now -name $camera-*.$format | sort -n); do
       echo "file '$file'" >> $workingDir$camera.txt
-      echo duration 0.2 >> $workingDir$camera.txt
+      echo duration $duration >> $workingDir$camera.txt
    done
 }
 
 createSequence() {
-    camera=$1
     /usr/bin/ffmpeg -f concat -i $workingDir$camera.txt -vsync vfr -pix_fmt yuv420p $workingDir$camera-output.mp4 -y
 }
 
 publish() {
-    camera=$1
     /bin/mv $workingDir$camera-output.mp4 $publishedDir/$camera.mp4
 }
 
 cleanUp() {
-    camera=$1
     /bin/rm $workingDir$camera.txt
 }
 
